5aa042a578c54bc259ad6ae469c5ff23750916b4
qicaisheng <stong@CNstong.local>
2016-09-24 14:42:42 +0800

    Parking lot should be not be able to park when parking lot is full

--- lib/parking_lot.rb
+++ lib/parking_lot.rb
@@ -1,9 +1,19 @@
 class ParkingLot
+  def initialize(capacity)
+    @capacity = capacity
+    @storage = []
+  end
+
   def park(car)
-    @car = car
+    return false unless canPark
+    @storage.push(car)
   end
 
   def pick
-    @car
+    @storage.pop
+  end
+
+  def canPark
+    @capacity > @storage.size
   end
 end
--- spec/parking_lot_spec.rb
+++ spec/parking_lot_spec.rb
@@ -4,13 +4,34 @@ require 'car'
 describe ParkingLot do
   context 'when a car park to the parking lot' do
     it 'should be able to pick the car' do
-      parking_lot = ParkingLot.new
+      parking_lot = ParkingLot.new(1)
       car = Car.new
 
       parking_lot.park(car)
 
       picked_car = parking_lot.pick
       expect(picked_car).to eq car
+    end
+  end
+
+  context 'when parking lot is full' do
+    before :each do
+      @parking_lot = ParkingLot.new(1)
+      @car1 = Car.new
+      @car2 = Car.new
+      @parking_lot.park(@car1)
+    end
+
+    it 'should be not able to park the car' do
+      expect(@parking_lot.park(@car2)).to be_falsy
+    end
+
+    it 'should be able to park the car after picking another car' do
+      @parking_lot.pick
+
+      @parking_lot.park(@car2)
+      picked_car = @parking_lot.pick
+      expect(picked_car).to eq @car2
     end
   end
 end
