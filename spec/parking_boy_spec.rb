92983411295c2d34b91e5e1ec88670909e8fa052
qicaisheng <qicaisheng@qq.com>
2016-09-24 15:59:39 +0800

    ParkingBoy should no be able to park the car after managed parking lots are full

--- spec/parking_boy_spec.rb
+++ spec/parking_boy_spec.rb
@@ -23,21 +23,38 @@ describe ParkingBoy do
       picked_car = @parking_lot.pick(token)
       expect(picked_car).to eq @car
     end
+
+    it 'should not be able to park the car when parking lot is full' do
+      @parking_lot.park(@car)
+
+      token = @parking_boy.park(Car.new)
+      expect(token).to be_falsy
+    end
   end
 
   context 'given the parking boy manage two parking lot' do
+    before :each do
+      @parking_lot1 = ParkingLot.new(1)
+      @parking_lot2 = ParkingLot.new(1)
+      @parking_boy = ParkingBoy.new(@parking_lot1, @parking_lot2)
+      @car1 = Car.new
+      @car2 = Car.new
+    end
+
     it 'should be park the car to another parking lot when a parking lot is full' do
-      parking_lot1 = ParkingLot.new(1)
-      parking_lot2 = ParkingLot.new(1)
-      parking_boy = ParkingBoy.new(parking_lot1, parking_lot2)
-      car1 = Car.new
-      car2 = Car.new
+      @parking_lot1.park(@car1)
 
-      parking_lot1.park(car1)
+      token = @parking_boy.park(@car2)
+      picked_car = @parking_lot2.pick(token)
+      expect(picked_car).to eq @car2
+    end
 
-      token = parking_boy.park(car2)
-      picked_car = parking_lot2.pick(token)
-      expect(picked_car).to eq car2
+    it 'should not be able to park the car when parking lots are full' do
+      @parking_lot1.park(@car1)
+      @parking_lot2.park(@car2)
+
+      token = @parking_boy.park(Car.new)
+      expect(token).to be_falsy
     end
   end
 end
